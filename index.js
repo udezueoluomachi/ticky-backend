7ac84f9f4002115996f3ab4512b23b7e0f618b7f
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2022-04-18 23:33:01 +0100

    safe

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 4000;
 const server = http.createServer(
     (req, res) => {
         res.writeHead(200,{
-            "Access-Control-Allow-Origin"       : "*"//`https://amabolearn.github.io`,
-           , "Acess-Control-Allow-Methods"       : "OPTIONS, POST, GET",
+            "Access-Control-Allow-Origin"       : `https://udezueoluomachi.github.io`,
+            "Acess-Control-Allow-Methods"       : "OPTIONS, POST, GET",
             "Access-Control-Max-Age"            : 2592000,
             "Access-Control-Request-Headers"    : "Content-Type"
         });
